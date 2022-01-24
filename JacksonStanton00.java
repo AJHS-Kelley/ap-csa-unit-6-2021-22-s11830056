d02305974637b101363c948ae1b03b18eabcddcf
Johann B <s11830056@students.duvalschools.org>
2022-01-24 08:49:03 -0500

    v0.1
    updated

--- JacksonStanton00.java
+++ JacksonStanton00.java
@@ -1,23 +1,18 @@
-public class Arrays {
+// Johann Burke, Array practice, v0.1, 01.24.22 //#endregion
+
+
+
+public class JacksonStanton00 {
 
     public static void main(String[]args) {
-        String[] strArr = new String[10];
+        String[] strArr = {"dog","cat","fish","dove","dragon"};
         int[] IntArr = {0, 1, 2, 3, 4, 5, 6, 7, 8 ,9};
         double[] doubArr = {1.5,2.6,7,8.9,9.5,10.2,12.3,16.3,20.6,22.4};
 
-        strArr[0] = "dog";
-        strArr[1] = "cat";
-        strArr[2] = "fish";
-        strArr[3] = "dove";
-        strArr[4] = "dragon";
-        strArr[5] = "parrot";
-        strArr[6] = "grasshopper";
-        strArr[7] = "wasp";
-        strArr[8] = "falcon";
-        strArr[9] = "eagle";
+
 
         
-        stringMethod(strArr);
+        
     }
 
 public static void stringMethod(){
@@ -25,9 +20,9 @@ for(int i = 0; i < 10; i++){
     String currentString = strArr[i];
     int stringLength = currentString.length();
     if(stringLength % 2 == 0)
-        System.out.printLn("the string length is even");
+        System.out.println("the string length is even");
     else if(stringLength % 2 == 1)
-    System.out.printLn("The String Length is odd.");
+    System.out.println("The String Length is odd.");
 
 }
 
@@ -45,7 +40,7 @@ public static void intMethod(int[] intArr){
 }
 
 
-}
+
 
 
 public static void doubleMethod(){
